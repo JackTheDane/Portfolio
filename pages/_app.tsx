912af7f619cf86c9a22af0bf88f440beed9878bb
Martin Bøje Petersen <mbp@peoplenet.dk>
2020-05-28 10:38:20 +0200

    Removed scroll tracker for performance issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,6 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { NavLink } from 'components/reusables/NavLink';
 import { SiteRoutes } from 'data/routes/SiteRoutes';
-import { useScrollPosition } from 'hooks/useScrollPosition';
-import { useState } from 'react';
-import { useIsomorphicLayoutEffect } from 'utils/SSR/useIsomorphicLayoutEffect';
 // const bgImage = require('images/bg.jpg?size=800');
 
 const AppCustomized = ({ Component, pageProps }: AppProps) => {
@@ -17,20 +14,6 @@ const AppCustomized = ({ Component, pageProps }: AppProps) => {
   const { pathname } = useRouter();
 
   const isFrontPage: boolean = pathname === `/${SiteRoutes.frontpage}`;
-
-  const [isMobileNavShown, setIsMobileNavShown] = useState<boolean>(false);
-
-  useScrollPosition(({
-    currPos
-  }) => {
-    const newVal: boolean = currPos.y > 0;
-
-    if (isMobileNavShown !== newVal) setIsMobileNavShown(newVal);
-  }, [isMobileNavShown, pathname], 50);
-
-  useIsomorphicLayoutEffect(() => {
-    if (window && window.scrollY > 0 && !isMobileNavShown) setIsMobileNavShown(true);
-  }, []);
 
   return (
     <>
@@ -43,12 +26,17 @@ const AppCustomized = ({ Component, pageProps }: AppProps) => {
       <div className={`${styles.app} ${isFrontPage ? styles.frontPage : '' }`}>
         <div className="pageBackground" />
 
-        <div className={`content ${styles.mobileTopNavBar} ${isMobileNavShown ? styles.shown : ''}`} >
-          <div className="pageBackground" style={{ position: 'absolute' }} />
-          <NavLink href="/" >
-            <img src={require('images/mainLogoOnly-white.png?size=60')} style={{ height: '100%' }} />
-          </NavLink>
-        </div>
+        {
+          !isFrontPage && (
+            <div className={`content ${styles.mobileTopNavBar}`} >
+              <div className="pageBackground" style={{ position: 'absolute' }} />
+              <NavLink href="/" >
+                <img src={require('images/mainLogoOnly-white.png?size=60')} style={{ height: '100%' }} />
+              </NavLink>
+            </div>
+          )
+        }
+
 
         <Nav />
 
