6653201c6931cf19641060e855a44be0dea2cc78
Martin Petersen <35668734+JackTheDane@users.noreply.github.com>
2020-08-28 23:24:35 +0200

    Removed duplicate manifest info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,18 +47,6 @@ const AppCustomized = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#175397" />
-        <meta name="theme-color" content="#175397" />
-        <meta name="msapplication-TileColor" content="#175397" />
-        <meta name="msapplication-navbutton-color" content="#175397" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-
 
         <title>
           Martin Bøje Petersen
