b65e60339b73dd6e04ad74702df2eff8fc4d6a06
jackthedane <fader4ever@hotmail.com>
2020-09-05 01:28:08 +0200

    Implemented basic progressive background image loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,12 @@ import Head from 'next/head';
 import Router, { useRouter } from 'next/router';
 import { SiteRoutes } from 'data/routes/SiteRoutes';
 import { useState, useEffect } from 'react';
-import { ProgressiveImageLoader } from 'components/reusables/ProgressiveImageLoader';
-// const bgImage = require('images/bg.jpg?size=800');
 
 const AppCustomized = ({ Component, pageProps }: AppProps) => {
 
   const { pathname } = useRouter();
   const [isRedirecting, setIsRedirecting] = useState<boolean>(false);
+  const [backgroundImage, setBackgroundImage] = useState<string>('');
 
   const onRouteChangeCompleted = (): void => {
     setIsRedirecting(false);
@@ -29,12 +28,21 @@ const AppCustomized = ({ Component, pageProps }: AppProps) => {
       Router.events.on('routeChangeStart', onRouteChangeStarted);
       Router.events.on('routeChangeComplete', onRouteChangeCompleted);
       Router.events.on('routeChangeError', onRouteChangeCompleted);
+
+      // Load image in background using dummy image
+      const newImg = new Image;
+      newImg.onload = function () { // Once loaded, set as background image
+        setTimeout(() => setBackgroundImage(newImg.src), 1000);
+      }
+      newImg.src = require('images/bg.jpg');
+
       return () => {
         Router.events.off('routeChangeStart', onRouteChangeStarted);
         Router.events.off('routeChangeComplete', onRouteChangeCompleted);
         Router.events.off('routeChangeError', onRouteChangeCompleted);
       }
     }, []);
+
 
   const isFrontPage: boolean = pathname === `/${SiteRoutes.frontpage}`;
 
@@ -45,13 +53,14 @@ const AppCustomized = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-
         <title>
           Martin Bøje Petersen
         </title>
       </Head>
+
       <div className={`${styles.app} ${isFrontPage ? styles.frontPage : ''}`}>
         <div className="pageBackground" />
+        <div className="pageBackground placeholder" style={{ opacity: backgroundImage ? 0 : 1 }} />
 
         <Nav />
 
@@ -71,7 +80,15 @@ const AppCustomized = ({ Component, pageProps }: AppProps) => {
         }
 
         .pageBackground::before {
-          background-image: url('${ require('images/bg.jpg?inline')}')
+          background-image: url('${backgroundImage}')
+        }
+
+        .pageBackground.placeholder {
+          transition: opacity .6s ease-in-out;
+        }
+
+        .pageBackground.placeholder::before {
+          background-image: url('${require('images/bg.jpg?lqip')}')
         }
       `}</style>
         <script></script>
